2f36d1c9bbc6b7cf3876fc0cac01ab4c5cbedcdc
Luodian <1121058986@qq.com>
2018-01-04 21:46:38 +0800

    修改了主页文件的路径
    Signed-off-by: Luodian <1121058986@qq.com>

--- src/main/java/com/company/main.java
+++ src/main/java/com/company/main.java
@@ -1,21 +1,53 @@
 package com.company;
 
 import org.apache.ignite.Ignite;
-import org.apache.ignite.IgniteCompute;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.IgniteTransactions;
 import org.apache.ignite.Ignition;
-import org.apache.ignite.compute.ComputeTaskFuture;
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.transactions.Transaction;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 class Main {
 	public static void main (String[] args) {
 		
-		try (Ignite ignite = Ignition.start ("examples/config/example-cache.xml")) {
-			// Limit broadcast to remote nodes only and
-			// enable asynchronous mode.
-			IgniteCompute compute = ignite.compute (ignite.cluster ().forRemotes ()).withAsync ();
-			// Print out hello message on remote nodes in the cluster group.
-			compute.broadcast (() -> System.out.println ("Hello Node: " + ignite.cluster ().localNode ().id ()));
-			ComputeTaskFuture<?> fut = compute.future ();
-			fut.listen (f -> System.out.println ("Finished sending broadcast job."));
+		try {
+			CacheConfiguration cacheCfg = new CacheConfiguration ("myCache");
+			cacheCfg.setCacheMode (CacheMode.PARTITIONED);
+			
+			IgniteConfiguration cfg = new IgniteConfiguration ();
+			cfg.setCacheConfiguration (cacheCfg);
+			cfg.setPeerClassLoadingEnabled (true);
+			
+			Ignite ignite = Ignition.start (cfg);
+			
+			IgniteTransactions transactions = ignite.transactions ();
+			
+			Transaction tx = transactions.txStart ();
+			
+			IgniteCache<String, byte[]> cache = ignite.cache ("myCache");
+			
+			
+			try (InputStream input = new FileInputStream ("/Users/luodian/Desktop/HCCP-Distributed-Download/src/main/resources/sample.jpg")) {
+				byte[] buf = new byte[35000000];
+				int bytesRead = 0;
+				// 2.read(byte b)
+				// bytesRead = input.read(buf);
+				// 3.read(byte b[], int off, int len)
+				bytesRead = input.read (buf, 0, 35000000);
+				
+				cache.put ("sample", buf);
+				
+			} catch (Exception e) {
+				e.printStackTrace ();
+			}
+			
+			// Add listener for unordered messages on all remote nodes.
+			tx.commit ();
 		} catch (Exception e) {
 			e.printStackTrace ();
 		}
