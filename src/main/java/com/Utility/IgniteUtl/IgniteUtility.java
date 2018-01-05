9db2767e37d390d819bd2e5881e339bcac3b5ac2
BIGKnight <309426989@qq.com>
2018-01-05 19:03:01 +0800

    123

--- src/main/java/com/Utility/IgniteUtl/IgniteUtility.java
+++ src/main/java/com/Utility/IgniteUtl/IgniteUtility.java
@@ -13,25 +13,20 @@ import java.util.Collection;
 import java.util.UUID;
 
 public class IgniteUtility {
-	public static void broadcast (Ignite ignite, Collection<UUID> sons_id, String url, long start, long end, int seriesID) {
-		try {
-			// Messaging instance over given cluster group (in this case, remote nodes).
-			if (sons_id.size () == 0) {
-				return;
-			}
-			
-			IgniteMessaging rmtMsg = ignite.message (ignite.cluster ().forRemotes ().forNodeIds (sons_id));
-			ArrayList<String> config = new ArrayList<> ();
-			
-			config.add (url);
-			config.add (String.valueOf (start));
-			config.add (String.valueOf (end));
-			config.add (String.valueOf (seriesID));
-			rmtMsg.sendOrdered ("DownloadTaskComing", config.toString (), 1);
-		} catch (Exception e) {
-			e.printStackTrace ();
-		}
-	}
+    public static void broadcast (Ignite ignite, UUID son_id, String url, long start, long end, int seriesID) {
+        try {
+            // Messaging instance over given cluster group (in this case, remote nodes).
+            IgniteMessaging rmtMsg = ignite.message (ignite.cluster ().forNodeId (son_id));
+            ArrayList<String> config = new ArrayList<> ();
+            config.add (url);
+            config.add (String.valueOf (start));
+            config.add (String.valueOf (end));
+            config.add (String.valueOf (seriesID));
+            rmtMsg.sendOrdered ("DownloadTaskComing", config.toString (), 1);
+        } catch (Exception e) {
+            e.printStackTrace ();
+        }
+    }
 	
 	public static Ignite startDefaultIgnite () {
 		CacheConfiguration cacheCfg = new CacheConfiguration("myCache");
