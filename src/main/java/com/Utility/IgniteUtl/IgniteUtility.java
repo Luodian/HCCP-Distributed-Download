5e60de9802819965948fbc4c26a3e1b6fa0cf967
Luodian <drluodian@gmail.com>
2018-01-05 17:19:19 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Luodian <1121058986@qq.com>
    
    # Conflicts:
    #	src/main/java/com/Utility/IgniteUtl/IgniteUtility.java

--- src/main/java/com/Utility/IgniteUtl/IgniteUtility.java
+++ src/main/java/com/Utility/IgniteUtl/IgniteUtility.java
@@ -1,7 +1,10 @@
 package com.Utility.IgniteUtl;
 
+import com.Utility.DownloadUtility.SiteFileFetch;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteMessaging;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.IgniteConfiguration;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -26,5 +29,41 @@ public class IgniteUtility {
 		} catch (Exception e) {
 			e.printStackTrace ();
 		}
+	}
+	
+	public static Ignite startDefaultIgnite () {
+//		CacheConfiguration cacheCfg = new CacheConfiguration ("Cache");
+//		cacheCfg.setCacheMode (CacheMode.PARTITIONED);
+		IgniteConfiguration cfg = new IgniteConfiguration ();
+//		cfg.setCacheConfiguration (cacheCfg);
+		cfg.setPeerClassLoadingEnabled (true);
+		Ignite ignite = Ignition.start (cfg);
+		IgniteMessaging igniteMessaging = ignite.message (ignite.cluster ().forLocal ());
+		//监听的消息是接受方而不是发送方，监听的是接受这个动作而不是发送这个东作，所以其实这里填写local就可以了
+		igniteMessaging.remoteListen ("DownloadTaskComing", (nodeID, msg) -> {
+			msg = msg.toString ().substring (1, msg.toString ().length () - 1);
+			String tmp[] = msg.toString ().split (",");
+			if (tmp.length != 3) {
+				System.err.println ("WRONG DOWNLOAD TASK COMING MESSAGE");
+				return true;
+			}
+			String url = tmp[0];
+			long start = Long.valueOf (tmp[1]);
+			long end = Long.valueOf (tmp[2]);
+			String seriesID = tmp[3];
+			SiteFileFetch siteFileFetch = new SiteFileFetch (url, "Tmp", nodeID + "_" + seriesID, start, end, 5);
+			try {
+				siteFileFetch.join ();
+				//当前版本一台机子只允许一个节点
+			} catch (InterruptedException e) {
+				e.printStackTrace ();
+			}
+			return true;
+		});
+		return ignite;
+	}
+	
+	public static void ConcateByteArray (ArrayList<byte[]> multi_file) {
+	
 	}
 }
