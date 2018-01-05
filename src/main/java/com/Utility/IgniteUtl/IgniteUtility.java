987dbf692c86ce6f44ef651ff439a79b890540e8
BIGKnight <309426989@qq.com>
2018-01-05 18:33:32 +0800

    cache1

--- src/main/java/com/Utility/IgniteUtl/IgniteUtility.java
+++ src/main/java/com/Utility/IgniteUtl/IgniteUtility.java
@@ -1,11 +1,13 @@
 package com.Utility.IgniteUtl;
 
 import com.Utility.DownloadUtility.SiteFileFetch;
-import org.apache.ignite.Ignite;
-import org.apache.ignite.IgniteMessaging;
-import org.apache.ignite.Ignition;
+import org.apache.ignite.*;
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.transactions.Transaction;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.UUID;
@@ -32,13 +34,21 @@ public class IgniteUtility {
 	}
 	
 	public static Ignite startDefaultIgnite () {
-//		CacheConfiguration cacheCfg = new CacheConfiguration ("Cache");
-//		cacheCfg.setCacheMode (CacheMode.PARTITIONED);
+		CacheConfiguration cacheCfg = new CacheConfiguration("myCache");
+		cacheCfg.setCacheMode (CacheMode.PARTITIONED);
 		IgniteConfiguration cfg = new IgniteConfiguration ();
-//		cfg.setCacheConfiguration (cacheCfg);
+		cfg.setCacheConfiguration (cacheCfg);
 		cfg.setPeerClassLoadingEnabled (true);
+
 		Ignite ignite = Ignition.start (cfg);
-		IgniteMessaging igniteMessaging = ignite.message (ignite.cluster ().forLocal ());
+
+		IgniteTransactions transactions = ignite.transactions ();
+
+		Transaction tx = transactions.txStart ();
+
+		IgniteCache<String, ArrayList<byte[]>> cache = ignite.cache ("myCache");
+
+		IgniteMessaging igniteMessaging = ignite.message (ignite.cluster ().forLocal());
 		//监听的消息是接受方而不是发送方，监听的是接受这个动作而不是发送这个东作，所以其实这里填写local就可以了
 		igniteMessaging.remoteListen ("DownloadTaskComing", (nodeID, msg) -> {
 			msg = msg.toString ().substring (1, msg.toString ().length () - 1);
@@ -54,7 +64,26 @@ public class IgniteUtility {
 			SiteFileFetch siteFileFetch = new SiteFileFetch (url, "Tmp", nodeID + "_" + seriesID, start, end, 5);
 			try {
 				siteFileFetch.join ();
-				//当前版本一台机子只允许一个节点
+                File file = new File("Tmp/" + nodeID + "_" + seriesID);
+                ArrayList<byte[]> tmpArrays = new ArrayList<byte[]>();
+                try (FileInputStream fileInputStream = new FileInputStream(file);
+                     BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream)
+                ){
+                    byte[] bytes = new byte[1024 * 1024];
+                    int is_end = bufferedInputStream.read(bytes);
+                    while (is_end != 1) {
+						tmpArrays.add(bytes);
+						bytes = new byte[1024 * 1024];
+						is_end = bufferedInputStream.read(bytes);
+					}
+					cache.put(seriesID, tmpArrays);
+                    igniteMessaging.send(seriesID, "SUCCESS");
+                }catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //当前版本一台机子只允许一个节点
 			} catch (InterruptedException e) {
 				e.printStackTrace ();
 			}
