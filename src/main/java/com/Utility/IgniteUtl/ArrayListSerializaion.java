e053c267570cbb8e298ed2487ea4c0aef9ccd7ab
Luodian <1121058986@qq.com>
2018-01-06 15:09:09 +0800

    更新文件2.0
    Signed-off-by: Luodian <1121058986@qq.com>

--- src/main/java/com/Utility/IgniteUtl/ArrayListSerializaion.java
+++ src/main/java/com/Utility/IgniteUtl/ArrayListSerializaion.java
@@ -5,8 +5,6 @@ import org.jetbrains.annotations.NotNull;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Vector;
-import java.util.Collection;
 
 /**
  * Created by 王烨臻 on 2018/1/6.
@@ -14,35 +12,6 @@ import java.util.Collection;
 public class ArrayListSerializaion<E> extends ArrayList<E> implements Externalizable {
 
     public ArrayListSerializaion() {
-        super();
-    }
-
-    public ArrayListSerializaion(@NotNull Collection<? extends E> collection) {
-        super(collection);
-    }
-
-    @Override
-    public void writeExternal(ObjectOutput objectOutput) throws IOException {
-       objectOutput.writeInt(size());
-       for (int i = 0;i<size();i++){
-           if (get(i) instanceof  Serializable){
-               objectOutput.writeObject(get(i));
-           }
-           else objectOutput.writeObject(null);
-       }
-    }
-
-    @Override
-    public void readExternal(ObjectInput objectInput) throws IOException, ClassNotFoundException {
-            int elementCount = objectInput.readInt();
-            this.ensureCapacity(elementCount);
-            for (int i = 0; i<elementCount;i++){
-                this.add((E)objectInput.readObject());
-            }
-    }
-public class ArrayListSerializaion<E> extends ArrayList<E> implements Externalizable {
-    
-    ArrayListSerializaion () {
         super();
     }
 
